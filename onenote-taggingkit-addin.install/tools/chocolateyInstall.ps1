b75c863890748f11890cfc02684e96d3ed8eae06
WetHat <PeterErnst@t-online.de>
2015-02-20 17:20:15 +0100

    chocolatey package for the OneNote tagging kit

---
+++ onenote-taggingkit-addin.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+$packageName   = 'onenote-taggingkit-addin.install'
+$installerType = 'MSI' #only one of these: exe, msi, msu
+$url           = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=onenotetaggingkit&DownloadId=926876&FileTime=130595054542130000&Build=20959'
+$silentArgs    = '/qn' 
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+# main helpers - these have error handling tucked into them already
+# installer, will assert administrative rights
+
+# if removing $url64, please remove from here
+Install-ChocolateyPackage $packageName $installerType $silentArgs "$url" -validExitCodes $validExitCodes
---
+++ onenote-taggingkit-addin.install/tools/chocolateyUninstall.ps1
@@ -0,0 +1,12 @@
+$namePattern = 'OneNote Tagging Kit'
+
+Get-WmiObject -Class 'Win32_Product' `
+| Where-Object { $_.Name -like $namePattern  } `
+| ForEach-Object {
+    $retcode = $_.Uninstall()
+    if ($retcode.ReturnValue -ne 0)
+    {
+      throw "Uninstallation of $($_.Name) - $($_.Version) failed with error code: $($retcode.ReturnValue)"
+    } 
+    Write-Host "Uninstalled $($_.Name) - $($_.Version)"
+  }
