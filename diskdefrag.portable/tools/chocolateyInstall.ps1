939adf8bf415a04b201485e289fae529b1202627
WetHat <PeterErnst@t-online.de>
2015-02-23 11:10:31 +0100

    auslogics disk defrag portable chocolatey package

---
+++ diskdefrag.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,56 @@
+﻿$packageName         = 'diskdefrag.portable'
+$url                 = 'http://www.auslogics.com/en/downloads/disk-defrag/ausdiskdefragportable.exe' # download url
+$installlocation     = Split-Path -parent $MyInvocation.MyCommand.Definition
+$shortcutLocation    = 'Microsoft\Windows\Start Menu\Programs\Chocolatey'
+$shortcutRegistry    = 'shortcuts.txt' # we register shortcuts for removal on Uninstall here
+$shortcutName        = 'Auslogics Disk Defrag.lnk'
+$shortcutDescription = 'Compact and fast harddisk defragmenter'
+$app                 = Join-Path -Path $installlocation -ChildPath 'AuslogicsDiskDefrag.exe'
+
+Get-ChocolateyWebFile $packageName $app $url 
+
+Get-ChildItem -Name $installlocation -filter '*.exe' -Recurse `
+| ForEach-Object {
+    [System.IO.FileInfo]$exe = Join-Path -Path $installlocation -ChildPath $_
+    if ($exe.BaseName -eq 'AuslogicsDiskDefrag')
+    {
+      echo '' >"$($exe.FullName).gui"
+
+      ## install a shortcut to the start menu to make this app discoverable
+      [string]$shortcutFolder = Join-Path -Path $env:ALLUSERSPROFILE `
+                                          -ChildPath $shortcutLocation 
+      [string]$shortcut       = Join-Path -Path $shortcutFolder `
+                                          -ChildPath $shortcutName
+      # register shortcut for removal on uninstall
+      Out-File -InputObject $shortcut `
+               -FilePath (Join-Path -Path $installlocation -ChildPath $shortcutRegistry)
+      if (![System.IO.Directory]::Exists( $shortcutFolder))
+      {
+        [System.IO.Directory]::CreateDirectory($shortcutFolder) >$null
+      }
+       <# TODO: use this when it becomes available in chocolatey
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcut `
+                                 -WorkingDirectory $exe.FullName
+                                 ...
+      #>
+      try
+      {
+          $wscript = New-Object -ComObject WScript.Shell
+          $lnk =  $wscript.CreateShortcut($shortcut)
+          $lnk.TargetPath       = $exe.FullName
+          $lnk.WorkingDirectory = $exe.DirectoryName
+          $lnk.Description      = $shortcutDescription
+          $lnk.Save()
+          Write-Host "Created Start Menu Shortcut: $shortcutname"
+      }
+      catch
+      {
+        Write-Host 'Shortcut creation failed..'
+        # It is not a showstopper, if shortcut creation fails
+      }
+    }
+    else
+    {
+       echo '' >"$($exe.FullName).ignore"
+    }
+  }
