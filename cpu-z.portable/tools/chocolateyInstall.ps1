9ebf86012b0ec65e4ed9579efb99ff2414667f0b
WetHat <PeterErnst@t-online.de>
2015-03-01 18:31:28 +0100

    cpu-z chocolatey package

---
+++ cpu-z.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,74 @@
+﻿$packageName         = 'cpu-z.portable' # arbitrary name for the package, used in messages
+$url                 = 'ftp://ftp.cpuid.com/cpu-z/cpu-z_1.72-en.zip' # download url
+$installlocation     = Split-Path -parent $MyInvocation.MyCommand.Definition
+$shortcutLocation    = 'Microsoft\Windows\Start Menu\Programs\Chocolatey'
+$shortcutRegistry    = 'shortcuts.txt' # we register shortcuts for removal on Uninstall here
+$shortcutDescription = 'Information on some of the main devices of your system'
+
+Install-ChocolateyZipPackage $packageName $url $installlocation
+
+$bitness = Get-ProcessorBits
+
+Get-ChildItem -Name $installlocation -filter '*.exe' -Recurse `
+| ForEach-Object {
+    [System.IO.FileInfo]$exe = Join-Path -Path $installlocation -ChildPath $_
+    
+    [bool]$publish = $false
+    [string]$shortcutName=''
+    
+    if ($exe.BaseName -eq 'cpuz_x64')
+    {
+      $shortcutName = 'CPU-Z System Information (64bit).lnk'
+      # we got the 64 bit executable
+      $publish = $bitness -eq 64
+    }
+    elseif ($exe.BaseName -eq 'cpuz_x32')
+    {
+      # we got the 32-bit executable
+      $shortcutName = 'CPU-Z System Information.lnk'
+      $publish = $bitness -eq 32
+    }
+    
+    if ($publish)
+    {
+      echo '' >"$($exe.FullName).gui"
+
+      ## install a shortcut to the start menu to make this app discoverable
+      [string]$shortcutFolder = Join-Path -Path $env:ALLUSERSPROFILE `
+                                          -ChildPath $shortcutLocation 
+      [string]$shortcut       = Join-Path -Path $shortcutFolder `
+                                          -ChildPath $shortcutName
+      # register shortcut for removal on uninstall
+      Out-File -InputObject $shortcut `
+               -Append `
+               -FilePath (Join-Path -Path $installlocation -ChildPath $shortcutRegistry)
+      if (![System.IO.Directory]::Exists( $shortcutFolder))
+      {
+        [System.IO.Directory]::CreateDirectory($shortcutFolder) >$null
+      }
+       <# TODO: use this when it becomes available in chocolatey
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcut `
+                                 -WorkingDirectory $exe.FullName
+                                 ...
+      #>
+      try
+      {
+          $wscript = New-Object -ComObject WScript.Shell
+          $lnk =  $wscript.CreateShortcut($shortcut)
+          $lnk.TargetPath       = $exe.FullName
+          $lnk.WorkingDirectory = $exe.DirectoryName
+          $lnk.Description      = $shortcutDescription
+          $lnk.Save()
+          Write-Host "Created Start Menu Shortcut: $shortcutname"
+      }
+      catch
+      {
+        Write-Host 'Shortcut creation failed..'
+        # It is not a showstopper, if shortcut creation fails
+      }
+    }
+    else
+    {
+       echo '' >"$($exe.FullName).ignore"
+    }
+  }
