69b609fd006746a137ec1d64bdf350d447c41f70
WetHat <PeterErnst@t-online.de>
2015-03-15 20:43:01 +0100

    portable chocolatey packages

---
+++ autoruns.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,44 @@
+﻿$packageName         = 'autoruns.portable' # arbitrary name for the package, used in messages
+$url                 = 'http://download.sysinternals.com/files/Autoruns.zip' # download url
+$shortcutLocation    = 'Microsoft\Windows\Start Menu\Programs\Chocolatey'
+$shortcutName        = 'Autoruns (Sysinternals).lnk'
+$shortcutDescription = 'Shows what programs are configured to run during system bootup or login'
+
+$appBase             = Split-Path -Parent `
+                                  -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$installlocation     = Join-Path -Path $appBase -ChildPath 'App'
+$shortcutRegistry    = Join-Path -Path $appBase -ChildPath 'shortcuts.txt'
+
+Install-ChocolateyZipPackage $packageName $url $installlocation
+
+Get-ChildItem -Name $installlocation -filter '*.exe' -Recurse `
+| ForEach-Object {
+    [System.IO.FileInfo]$exe = Join-Path -Path $installlocation -ChildPath $_
+    if ($exe.BaseName -eq 'autoruns')
+    {
+      echo '' >"$($exe.FullName).gui"
+
+      ## install a shortcut to the start menu to make this app discoverable
+      [string]$shortcutFolder = Join-Path -Path $env:ALLUSERSPROFILE `
+                                          -ChildPath $shortcutLocation 
+      [string]$shortcut       = Join-Path -Path $shortcutFolder `
+                                          -ChildPath $shortcutName
+      # register shortcut for removal on uninstall
+      Out-File -InputObject $shortcut `
+               -Append `
+               -FilePath $shortcutRegistry
+      if (![System.IO.Directory]::Exists( $shortcutFolder))
+      {
+        [System.IO.Directory]::CreateDirectory($shortcutFolder) >$null
+      }
+      
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcut `
+                                 -Targetpath $exe.FullName `
+                                 -WorkingDirectory $exe.DirectoryName `
+                                 -Description $shortcutDescription
+    }
+    else
+    {
+       echo '' >"$($exe.FullName).ignore"
+    }
+  }
---
+++ windlg.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,44 @@
+﻿$packageName         = 'windlg.portable' # arbitrary name for the package, used in messages
+$url                 = 'http://download.wdc.com/windlg/WinDlg_v1_28.zip' # download url
+$shortcutLocation    = 'Microsoft\Windows\Start Menu\Programs\Chocolatey'
+$shortcutName        = 'Data Lifeguard Diagnostics.lnk'
+$shortcutDescription = 'WinDlg_v1_28.zip'
+
+$appBase             = Split-Path -Parent `
+                                  -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$installlocation     = Join-Path -Path $appBase -ChildPath 'App'
+$shortcutRegistry    = Join-Path -Path $appBase -ChildPath 'shortcuts.txt'
+
+Install-ChocolateyZipPackage $packageName $url $installlocation
+
+Get-ChildItem -Name $installlocation -filter '*.exe' -Recurse `
+| ForEach-Object {
+    [System.IO.FileInfo]$exe = Join-Path -Path $installlocation -ChildPath $_
+    if ($exe.BaseName -eq 'WinDlg')
+    {
+      echo '' >"$($exe.FullName).gui"
+
+      ## install a shortcut to the start menu to make this app discoverable
+      [string]$shortcutFolder = Join-Path -Path $env:ALLUSERSPROFILE `
+                                          -ChildPath $shortcutLocation 
+      [string]$shortcut       = Join-Path -Path $shortcutFolder `
+                                          -ChildPath $shortcutName
+      # register shortcut for removal on uninstall
+      Out-File -InputObject $shortcut `
+               -Append `
+               -FilePath $shortcutRegistry
+      if (![System.IO.Directory]::Exists( $shortcutFolder))
+      {
+        [System.IO.Directory]::CreateDirectory($shortcutFolder) >$null
+      }
+      
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcut `
+                                 -Targetpath $exe.FullName `
+                                 -WorkingDirectory $exe.DirectoryName `
+                                 -Description $shortcutDescription
+    }
+    else
+    {
+       echo '' >"$($exe.FullName).ignore"
+    }
+  }
