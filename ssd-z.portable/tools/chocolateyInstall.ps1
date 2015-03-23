db2d29a6599b684822621cbb14f8d31b911c5844
WetHat <PeterErnst@t-online.de>
2015-03-23 13:34:41 +0100

    SSD-Z portable chocolatey package

---
+++ ssd-z.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,44 @@
+﻿$packageName         = 'ssd-z.portable' # arbitrary name for the package, used in messages
+$url                 = 'http://www.techpowerup.com/forums/attachments/ssd-z_15-03-15wip-zip.63399/' # download url
+$shortcutLocation    = 'Chocolatey'
+$shortcutName        = 'SSD-Z.lnk'
+$shortcutDescription = 'Detailed info about your solid-state disks and harddriveschoco pack'
+
+$appBase             = Split-Path -Parent `
+                                  -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$installlocation     = Join-Path -Path $appBase -ChildPath 'App'
+$shortcutRegistry    = Join-Path -Path $appBase -ChildPath 'shortcuts.txt'
+
+Install-ChocolateyZipPackage $packageName $url $installlocation
+
+Get-ChildItem -Name $installlocation -filter '*.exe' -Recurse `
+| ForEach-Object {
+    [System.IO.FileInfo]$exe = Join-Path -Path $installlocation -ChildPath $_
+    if ($exe.BaseName -eq 'SSD-Z')
+    {
+      echo '' >"$($exe.FullName).gui"
+
+      ## install a shortcut to the start menu to make this app discoverable
+      [string]$shortcutFolder = Join-Path -Path ([environment]::GetFolderPath([environment+specialfolder]::Programs)) `
+                                          -ChildPath $shortcutLocation 
+      [string]$shortcut       = Join-Path -Path $shortcutFolder `
+                                          -ChildPath $shortcutName
+      # register shortcut for removal on uninstall
+      Out-File -InputObject $shortcut `
+               -Append `
+               -FilePath $shortcutRegistry
+      if (![System.IO.Directory]::Exists( $shortcutFolder))
+      {
+        [System.IO.Directory]::CreateDirectory($shortcutFolder) >$null
+      }
+      
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcut `
+                                 -Targetpath $exe.FullName `
+                                 -WorkingDirectory $exe.DirectoryName `
+                                 -Description $shortcutDescription
+    }
+    else
+    {
+       echo '' >"$($exe.FullName).ignore"
+    }
+  }
