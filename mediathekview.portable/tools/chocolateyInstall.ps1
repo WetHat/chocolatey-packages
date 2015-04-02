2f65ed816beb8bcb591f176bece08c048a10347a
WetHat <PeterErnst@t-online.de>
2015-04-02 18:45:58 +0200

    portable chocolatey package

---
+++ mediathekview.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,44 @@
+﻿$packageID           = 'mediathekview.portable' # nuget package id
+$url                 = 'http://softlayer-ams.dl.sourceforge.net/project/zdfmediathk/Mediathek/Mediathek%209/MediathekView_9.zip' # download url
+$shortcutLocation    = 'Chocolatey'
+$shortcutName        = 'Mediathek View.lnk'
+$shortcutDescription = 'Online-Mediathek Viewer deutschprachiger TV Sender'
+
+$appBase             = Split-Path -Parent `
+                                  -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$installlocation     = Join-Path -Path $appBase -ChildPath 'App'
+$shortcutRegistry    = Join-Path -Path $appBase -ChildPath 'shortcuts.txt'
+
+Install-ChocolateyZipPackage $packageID $url $installlocation
+
+Get-ChildItem -Name $installlocation -filter '*.exe' -Recurse `
+| ForEach-Object {
+    [System.IO.FileInfo]$exe = Join-Path -Path $installlocation -ChildPath $_
+    if ($exe.BaseName -eq 'MediathekView__Start')
+    {
+      echo '' >"$($exe.FullName).gui"
+
+      ## install a shortcut to the start menu to make this app discoverable
+      [string]$shortcutFolder = Join-Path -Path ([environment]::GetFolderPath([environment+specialfolder]::Programs)) `
+                                          -ChildPath $shortcutLocation 
+      [string]$shortcut       = Join-Path -Path $shortcutFolder `
+                                          -ChildPath $shortcutName
+      # register shortcut for removal on uninstall
+      Out-File -InputObject $shortcut `
+               -Append `
+               -FilePath $shortcutRegistry
+      if (![System.IO.Directory]::Exists( $shortcutFolder))
+      {
+        [System.IO.Directory]::CreateDirectory($shortcutFolder) >$null
+      }
+      
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcut `
+                                 -Targetpath $exe.FullName `
+                                 -WorkingDirectory $exe.DirectoryName `
+                                 -Description $shortcutDescription
+    }
+    else
+    {
+       echo '' >"$($exe.FullName).ignore"
+    }
+  }
