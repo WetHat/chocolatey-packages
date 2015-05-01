047fe294667a9986c144170a7c6b6d1b07ebba11
WetHat <PeterErnst@t-online.de>
2015-05-01 11:50:08 +0200

    grepwin chocolatey package

---
+++ grepwin.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,49 @@
+﻿$packageID           = 'grepwin.portable' # nuget package id
+$url                 = 'http://sourceforge.net/projects/grepwin/files/1.6.8/grepWin-1.6.8_portable.exe/download' # download url
+$url64               = 'http://sourceforge.net/projects/grepwin/files/1.6.8/grepWin64-1.6.8_portable.exe/download' # download url
+$shortcutLocation    = 'Chocolatey'
+$shortcutName        = 'grepWin.lnk'
+$shortcutDescription = 'Regular expression search and replace for Windows'
+
+$appBase             = Split-Path -Parent `
+                                  -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$installlocation     = Join-Path -Path $appBase -ChildPath 'App'
+$shortcutRegistry    = Join-Path -Path $appBase -ChildPath 'shortcuts.txt'
+$app                 = Join-Path -Path $installlocation -ChildPath 'grepWin.exe'
+
+Get-ChocolateyWebFile  -packageName  $packageName `
+                       -fileFullPath $app `
+                       -url $url `
+                       -url64bit $url64
+
+Get-ChildItem -Name $installlocation -filter '*.exe' -Recurse `
+| ForEach-Object {
+    [System.IO.FileInfo]$exe = Join-Path -Path $installlocation -ChildPath $_
+    if ($exe.BaseName -eq 'grepWin')
+    {
+      echo '' >"$($exe.FullName).gui"
+
+      ## install a shortcut to the start menu to make this app discoverable
+      [string]$shortcutFolder = Join-Path -Path ([environment]::GetFolderPath([environment+specialfolder]::Programs)) `
+                                          -ChildPath $shortcutLocation 
+      [string]$shortcut       = Join-Path -Path $shortcutFolder `
+                                          -ChildPath $shortcutName
+      # register shortcut for removal on uninstall
+      Out-File -InputObject $shortcut `
+               -Append `
+               -FilePath $shortcutRegistry
+      if (![System.IO.Directory]::Exists( $shortcutFolder))
+      {
+        [System.IO.Directory]::CreateDirectory($shortcutFolder) >$null
+      }
+      
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcut `
+                                 -Targetpath $exe.FullName `
+                                 -WorkingDirectory $exe.DirectoryName `
+                                 -Description $shortcutDescription
+    }
+    else
+    {
+       echo '' >"$($exe.FullName).ignore"
+    }
+  }
