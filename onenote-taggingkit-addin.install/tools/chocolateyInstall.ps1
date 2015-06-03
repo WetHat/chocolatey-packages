e8728958a370fc54d5a08094b0ba7374cd530cc5
WetHat <PeterErnst@t-online.de>
2015-06-03 19:14:41 +0200

    app update

--- onenote-taggingkit-addin.install/tools/chocolateyInstall.ps1
+++ onenote-taggingkit-addin.install/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName   = 'onenote-taggingkit-addin.install'
 $installerType = 'MSI' #only one of these: exe, msi, msu
-$url           = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=onenotetaggingkit&DownloadId=926876&FileTime=130595054542130000'
+$url           = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=onenotetaggingkit&DownloadId=1459876&FileTime=130778237038200000'
 $silentArgs    = '/qn' 
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
