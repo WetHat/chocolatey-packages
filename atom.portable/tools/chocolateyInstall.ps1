9923101b398e85d66ad5127168aa846448ae431b
WetHat <PeterErnst@t-online.de>
2015-09-08 08:38:47 +0200

    App Updates

--- atom.portable/tools/chocolateyInstall.ps1
+++ atom.portable/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageID           = 'atom.portable'
-$url                 = 'https://github.com/atom/atom/releases/download/v1.0.9/atom-windows.zip' # download url
+$url                 = 'https://github.com/atom/atom/releases/download/v1.0.11/atom-windows.zip' # download url
 $shortcutLocation    = 'Chocolatey'
 $shortcutName        = 'Atom Text Editor.lnk'
 $shortcutDescription = 'Hackable text editor for the 21st Century'
