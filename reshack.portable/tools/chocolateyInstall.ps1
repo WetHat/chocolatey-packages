e67e2ea4907bfdd039921218c26379dd7b6a1aac
WetHat <PeterErnst@t-online.de>
2015-10-11 12:47:07 +0200

    Initial portable version of reshack

---
+++ reshack.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,44 @@
+﻿$packageID           = 'reshack.portable.4.2.5' # nuget package id includes version because URL does not
+$url                 = 'http://www.angusj.com/resourcehacker/resource_hacker.zip' # download url
+$shortcutLocation    = 'Chocolatey'
+$shortcutName        = 'Resource Hacker.lnk'
+$shortcutDescription = 'Resource editor for Windows executables'
+
+$appBase             = Split-Path -Parent `
+                                  -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$installlocation     = Join-Path -Path $appBase -ChildPath 'App'
+$shortcutRegistry    = Join-Path -Path $appBase -ChildPath 'shortcuts.txt'
+
+Install-ChocolateyZipPackage $packageID $url $installlocation
+
+Get-ChildItem -Name $installlocation -filter '*.exe' -Recurse `
+| ForEach-Object {
+    [System.IO.FileInfo]$exe = Join-Path -Path $installlocation -ChildPath $_
+    if ($exe.BaseName -eq 'ResourceHacker')
+    {
+      echo '' >"$($exe.FullName).gui"
+
+      ## install a shortcut to the start menu to make this app discoverable
+      [string]$shortcutFolder = Join-Path -Path ([environment]::GetFolderPath([environment+specialfolder]::Programs)) `
+                                          -ChildPath $shortcutLocation
+      [string]$shortcut       = Join-Path -Path $shortcutFolder `
+                                          -ChildPath $shortcutName
+      # register shortcut for removal on uninstall
+      Out-File -InputObject $shortcut `
+               -Append `
+               -FilePath $shortcutRegistry
+      if (![System.IO.Directory]::Exists( $shortcutFolder))
+      {
+        [System.IO.Directory]::CreateDirectory($shortcutFolder) >$null
+      }
+
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcut `
+                                 -Targetpath $exe.FullName `
+                                 -WorkingDirectory $exe.DirectoryName `
+                                 -Description $shortcutDescription
+    }
+    else
+    {
+       echo '' >"$($exe.FullName).ignore"
+    }
+  }
