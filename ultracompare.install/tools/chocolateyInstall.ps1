bbef10108949362a3cf39e1a4fe04c3cad93db7e
WetHat <PeterErnst@t-online.de>
2015-12-21 11:02:34 +0100

    App Updates

--- ultracompare.install/tools/chocolateyInstall.ps1
+++ ultracompare.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-﻿$packageName    = 'ultracompare.install.15.10'
+﻿$packageName    = 'ultracompare.install.15.20'
 $installerType  = 'MSI' #only one of these: exe, msi, msu
 $url            = 'http://www.ultraedit.com/files/msii/uc_english.msi' # download url
-$silentArgs     = '/qn /norestart' 
-$validExitCodes = @(0) 
+$silentArgs     = '/qn /norestart'
+$validExitCodes = @(0)
 $installlocation= Split-Path -parent $MyInvocation.MyCommand.Definition
 
 ## Make sure new installer is cached by adding version
