45049cde0a8926e45248b5af8dd726aff13b15b2
WetHat <PeterErnst@t-online.de>
2016-01-05 10:29:00 +0100

    App UPdates

--- calibre.portable/tools/chocolateyInstall.ps1
+++ calibre.portable/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName         = 'calibre.portable' # arbitrary name for the package, used in messages
-$url                 = 'http://download.calibre-ebook.com/2.47.0/calibre-portable-installer-2.47.0.exe' # download url
+$url                 = 'http://download.calibre-ebook.com/2.48.0/calibre-portable-installer-2.48.0.exe' # download url
 $shortcutLocation    = 'Chocolatey'
 $shortcutName        = 'Calibre E-Book Manager.lnk'
 $shortcutDescription = 'Calibre e-book library manager'
