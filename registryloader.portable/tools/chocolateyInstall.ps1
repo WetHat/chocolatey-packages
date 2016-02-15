6b9d9b9ce7031127d9cc3747854ad8cad48703ea
WetHat <PeterErnst@t-online.de>
2016-02-15 10:07:56 +0100

    Registry loader initial version

---
+++ registryloader.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,44 @@
+﻿$packageID           = 'registryloader.portable.1.0.19' # nuget package id with version added because URL is not version dependent
+$url                 = 'http://www.wieldraaijer.nl/download/RegistryLoaderPE/RegistryLoaderPE.exe' # download url
+$shortcutLocation    = 'Chocolatey'
+$shortcutName        = 'RegistryLoaderPE.lnk'
+$shortcutDescription = 'Load registry from offline systems'
+
+$appBase             = Split-Path -Parent `
+                                  -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$installlocation     = Join-Path -Path $appBase -ChildPath 'App'
+$shortcutRegistry    = Join-Path -Path $appBase -ChildPath 'shortcuts.txt'
+$app                 = Join-Path -Path $installlocation -ChildPath 'RegistryLoaderPE.exe'
+Get-ChocolateyWebFile $packageName $app $url
+
+Get-ChildItem -Name $installlocation -filter '*.exe' -Recurse `
+| ForEach-Object {
+    [System.IO.FileInfo]$exe = Join-Path -Path $installlocation -ChildPath $_
+    if ($exe.BaseName -eq 'RegistryLoaderPE')
+    {
+      echo '' >"$($exe.FullName).gui"
+
+      ## install a shortcut to the start menu to make this app discoverable
+      [string]$shortcutFolder = Join-Path -Path ([environment]::GetFolderPath([environment+specialfolder]::Programs)) `
+                                          -ChildPath $shortcutLocation 
+      [string]$shortcut       = Join-Path -Path $shortcutFolder `
+                                          -ChildPath $shortcutName
+      # register shortcut for removal on uninstall
+      Out-File -InputObject $shortcut `
+               -Append `
+               -FilePath $shortcutRegistry
+      if (![System.IO.Directory]::Exists( $shortcutFolder))
+      {
+        [System.IO.Directory]::CreateDirectory($shortcutFolder) >$null
+      }
+      
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcut `
+                                 -Targetpath $exe.FullName `
+                                 -WorkingDirectory $exe.DirectoryName `
+                                 -Description $shortcutDescription
+    }
+    else
+    {
+       echo '' >"$($exe.FullName).ignore"
+    }
+  }
