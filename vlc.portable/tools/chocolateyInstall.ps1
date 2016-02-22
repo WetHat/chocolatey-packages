47c69cc21f25acba2d5b08cbac36991404f18fc6
WetHat <PeterErnst@t-online.de>
2016-02-22 13:57:15 +0100

    App Update

--- vlc.portable/tools/chocolateyInstall.ps1
+++ vlc.portable/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName         = 'vlc.portable' # arbitrary name for the package, used in messages
-$url                 = 'http://mirror.de.leaseweb.net/videolan/vlc/2.2.1/win32/vlc-2.2.1-win32.zip' # download url
+$url                 = 'http://mirror.de.leaseweb.net/videolan/vlc/2.2.2/win32/vlc-2.2.2-win32.zip' # download url
 $shortcutLocation    = 'Chocolatey'
 $shortcutName        = 'VLC Media Player.lnk'
 $shortcutDescription = 'Free and open source cross-platform multimedia player.'
@@ -20,7 +20,7 @@ Get-ChildItem -Name $installlocation -filter '*.exe' -Recurse `
 
       ## install a shortcut to the start menu to make this app discoverable
       [string]$shortcutFolder = Join-Path -Path "$($env:APPDATA)\Microsoft\Windows\Start Menu\Programs" `
-                                          -ChildPath $shortcutLocation 
+                                          -ChildPath $shortcutLocation
       [string]$shortcut       = Join-Path -Path $shortcutFolder `
                                           -ChildPath $shortcutName
       # register shortcut for removal on uninstall
