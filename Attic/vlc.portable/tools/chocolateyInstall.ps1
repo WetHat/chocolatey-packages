26a6debee9e327e64e4cee4a714cf3306541ab33
WetHat <Peter.Ernst@cadm-inc-us.com>
2018-03-18 09:28:41 +0100

    vlc.portable decommissioned

---
+++ Attic/vlc.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,48 @@
+﻿$packageName         = 'vlc.portable' # arbitrary name for the package, used in messages
+$url                 = 'http://mirror.de.leaseweb.net/videolan/vlc/3.0.1/win32/vlc-3.0.1-win32.zip' # download url
+$shortcutLocation    = 'Chocolatey'
+$shortcutName        = 'VLC Media Player.lnk'
+$shortcutDescription = 'Free and open source cross-platform multimedia player.'
+
+$appBase             = Split-Path -Parent `
+                                  -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$installlocation     = Join-Path -Path $appBase -ChildPath 'App'
+$shortcutRegistry    = Join-Path -Path $appBase -ChildPath 'shortcuts.txt'
+
+Install-ChocolateyZipPackage -packageName   $packageName `
+                             -Url           $url `
+                             -UnzipLocation $installlocation `
+                             -Checksum      'A5255B3193DA9AB0842F78656BA90838E3B301BB283456F2D36706D6BE6BBD36' `
+                             -ChecksumType  'sha256'
+
+
+Get-ChildItem -Name $installlocation -filter '*.exe' -Recurse `
+| ForEach-Object {
+    [System.IO.FileInfo]$exe = Join-Path -Path $installlocation -ChildPath $_
+    if ($exe.BaseName -eq 'vlc')
+    {
+      echo '' >"$($exe.FullName).gui"
+
+      ## install a shortcut to the start menu to make this app discoverable
+      [string]$shortcutFolder = Join-Path -Path "$($env:APPDATA)\Microsoft\Windows\Start Menu\Programs" `
+                                          -ChildPath $shortcutLocation
+      [string]$shortcut       = Join-Path -Path $shortcutFolder `
+                                          -ChildPath $shortcutName
+      # register shortcut for removal on uninstall
+      Out-File -InputObject $shortcut `
+               -Append `
+               -FilePath $shortcutRegistry
+      if (![System.IO.Directory]::Exists( $shortcutFolder))
+      {
+        [System.IO.Directory]::CreateDirectory($shortcutFolder) >$null
+      }
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcut `
+                                 -Targetpath $exe.FullName `
+                                 -WorkingDirectory $exe.DirectoryName `
+                                 -Description $shortcutDescription
+    }
+    else
+    {
+       echo '' >"$($exe.FullName).ignore"
+    }
+  }
