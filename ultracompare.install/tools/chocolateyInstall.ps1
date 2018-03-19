8ec18c574ea8c2a91b8878c191d114930f8dab32
WetHat <Peter.Ernst@cadm-inc-us.com>
2018-03-19 09:14:28 +0100

    App update

--- ultracompare.install/tools/chocolateyInstall.ps1
+++ ultracompare.install/tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
 ﻿$packageName    = 'ultracompare.install'
 $installerType  = 'MSI' #only one of these: exe, msi, msu
-$url            = 'http://www.ultraedit.com/files/msii/uc_english.msi' # download url
+$url            = 'https://downloads.ultraedit.com/main/uc/win/uc_english.msi' # download 
+$url64          = 'https://downloads.ultraedit.com/main/uc/win/uc_english_64.msi' # download url
 $silentArgs     = '/qn /norestart'
 $validExitCodes = @(0)
 $installlocation= Split-Path -parent $MyInvocation.MyCommand.Definition
@@ -9,6 +10,9 @@ Install-ChocolateyPackage -packageName   $packageName `
                           -FileType      'MSI'        `
                           -SilentArgs     $silentArgs `
                           -Url            $url `
-                          -Checksum       '57DDD8222136674B996387BBC7FED412200429C7BB704C883E365723923C3144' `
+                          -Url64          $url64 `
+                          -Checksum       'B96EDC4DA9140C4EEE6D761085CDC8DA64A962CA08581594A8FB4AC8F13CDF79' `
+                          -Checksum64     'A6852713ED9FF62E46D1113622B5A6B8BA58BD689E37DEFC9EA2A67886C937C7' `
                           -ChecksumType   'sha256' `
+                          -ChecksumType64 'sha256' `
                           -validExitCodes $validExitCodes
