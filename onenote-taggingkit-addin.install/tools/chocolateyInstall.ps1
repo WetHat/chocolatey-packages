384550a57bc75a5ff96075ed1e12f82db1c6bfa3
WetHat <PeterErnst@outlook.de>
2022-04-25 19:03:45 +0200

    App Updata

--- onenote-taggingkit-addin.install/tools/chocolateyInstall.ps1
+++ onenote-taggingkit-addin.install/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName   = 'onenote-taggingkit-addin.install'
-$url           = 'https://github.com/WetHat/OnenoteTaggingKit/releases/download/v4.0.81360/SetupTaggingKitWiX.4.0.81360.38002.msi'
+$url           = 'https://github.com/WetHat/OnenoteTaggingKit/releases/download/v4.0.81500/SetupTaggingKitWiX.4.0.81500.15722.msi'
 $silentArgs    = '/qn' 
 $validExitCodes = @(0,1603) 
 
@@ -7,6 +7,6 @@ Install-ChocolateyPackage -packageName   $packageName `
                           -FileType      'MSI'        `
                           -SilentArgs     $silentArgs `
                           -Url            $url `
-                          -Checksum       'fc929c93bc688705c0667f10e449dbf4390b55da0d10dca520b83524f540a75e' `
+                          -Checksum       '9350067a7b98eece63d24e46ffa60fcb648ab7059ff66eb905404a4c99fd2376' `
                           -ChecksumType   'sha256' `
                           -validExitCodes $validExitCodes
