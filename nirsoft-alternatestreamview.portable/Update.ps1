c2461e91024faf58e37dcbee2a6e9d3a5b21ff1f
WetHat <PeterErnst@outlook.de>
2023-01-28 16:14:56 +0100

    alternatestreamview added

---
+++ nirsoft-alternatestreamview.portable/Update.ps1
@@ -0,0 +1,40 @@
+import-module au
+$releases = 'https://www.nirsoft.net/utils/alternate_data_streams.html'
+
+function global:au_SearchReplace {
+    @{
+       '.\tools\chocolateyInstall.ps1' = @{
+            '(?i)(\s*\$url32\s*=\s*'')[^'']+' = "`${1}$($Latest.URL32)" 
+            '(?i)(\s*\$url64\s*=\s*'')[^'']+' = "`${1}$($Latest.URL64)"
+            '(?i)(-Checksum\s*'')[^'']*'      = "`${1}$($Latest.Checksum32)"
+            '(?i)(-Checksum64\s*'')[^'']*'    = "`${1}$($Latest.Checksum64)"
+        }
+     }
+}
+
+function global:au_GetLatest {
+
+    $downloadPage = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $version = [regex]::Match($DownloadPage.RawContent,'(?<=AlternateStreamView\s*v\s*)\d+[.\d]+').Value
+    
+    # unversioned url 
+    $url32 = $downloadPage.links `
+    | ForEach-Object { $_.href } `
+    | Where-Object { $_ -match 'alternatestreamview\.zip' } `
+    | Select-Object -First 1
+
+    $url64 = $downloadPage.links `
+    | ForEach-Object { $_.href } `
+    | Where-Object { $_ -match 'alternatestreamview-x64\.zip' } `
+    | Select-Object -First 1
+    
+    @{ 
+        URL32 = "https://www.nirsoft.net/utils/$url32"
+        ChecksumType32 = 'sha256'
+        URL64 = "https://www.nirsoft.net/utils/$url64"
+        ChecksumType64 = 'sha256'
+        Version = $version }
+}
+
+update-package -ChecksumFor all  # do not call chocolateyInstall to genereate checksum
---
+++ nirsoft-alternatestreamview.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,51 @@
+﻿$packageName         = 'nirsoft-alternatestreamview.portable'
+$url32               = 'https://www.nirsoft.net/utils/alternatestreamview.zip' # download url
+$url64               = 'https://www.nirsoft.net/utils/alternatestreamview-x64.zip' # download url
+$shortcutLocation    = 'Chocolatey'
+$shortcutName        = 'NTFSlinksView.lnk'
+$shortcutDescription = 'Shows a list of all symbolic links and junctions in the specified folder, and their target paths'
+
+$appBase             = Split-Path -Parent `
+                                  -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$installlocation     = Join-Path -Path $appBase -ChildPath 'App'
+$shortcutRegistry    = Join-Path -Path $appBase -ChildPath 'shortcuts.txt'
+
+Install-ChocolateyZipPackage -PackageName    $packageName `
+                             -Url            $url32 `
+                             -Checksum      '6a4600134b0b3f641b5b2694315c32db71f11b2d22aaeac027ae55101033d0b1' `
+                             -ChecksumType  'sha256' `
+                             -Url64bit       $url64 `
+                             -ChecksumType64 'sha256' `
+                             -Checksum64     'ad1f0edb2202ec1a736d9266b18b241eac5542c350578a3a8cdb4dd8b14ff37d' `
+                             -UnzipLocation $installlocation
+
+Get-ChildItem -Name $installlocation -filter '*.exe' -Recurse `
+| ForEach-Object {
+    [System.IO.FileInfo]$exe = Join-Path -Path $installlocation -ChildPath $_
+    if ($exe.BaseName -eq 'AlternateStreamView')
+    {
+      echo '' >"$($exe.FullName).gui"
+
+      ## install a shortcut to the start menu to make this app discoverable
+      [string]$shortcutFolder = Join-Path -Path "$($env:APPDATA)\Microsoft\Windows\Start Menu\Programs" `
+                                          -ChildPath $shortcutLocation
+      [string]$shortcut       = Join-Path -Path $shortcutFolder `
+                                          -ChildPath $shortcutName
+      # register shortcut for removal on uninstall
+      Out-File -InputObject $shortcut `
+               -Append `
+               -FilePath $shortcutRegistry
+      if (![System.IO.Directory]::Exists( $shortcutFolder))
+      {
+        [System.IO.Directory]::CreateDirectory($shortcutFolder) >$null
+      }
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcut `
+                                 -Targetpath $exe.FullName `
+                                 -WorkingDirectory $exe.DirectoryName `
+                                 -Description $shortcutDescription
+    }
+    else
+    {
+       echo '' >"$($exe.FullName).ignore"
+    }
+  }
