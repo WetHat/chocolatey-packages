87fae05725daea387f0cf7d10e825f638f5b7457
WetHat <PeterErnst@outlook.de>
2026-04-05 19:38:09 +0200

    App update

--- sbcl/tools/chocolateyinstall.ps1
+++ sbcl/tools/chocolateyinstall.ps1
@@ -4,9 +4,9 @@ $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
-  url           = 'http://prdownloads.sourceforge.net/sbcl/sbcl-2.6.2-x86-64-windows-binary.msi'
+  url           = 'http://prdownloads.sourceforge.net/sbcl/sbcl-2.6.3-x86-64-windows-binary.msi'
   softwareName  = 'Steel Bank Common Lisp*'
-  checksum      = '974848ce053d10d25ad4c4668fd1b9241abed3843dfe8d0c9aa07876ca401584'
+  checksum      = 'ff265a4c3f3c02b412e4cb1e6bfbbe7f9e87683fa1970c5d311cf10f5d1aa889'
   checksumType  = 'sha256'
 
   # MSI
